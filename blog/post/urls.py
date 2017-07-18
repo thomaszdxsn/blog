60d5f96ae2e3b86d139be6aa8b07fc2fb05d4395
周杨 <bnm_965321@sina.com>
2017-07-18 16:34:11 +0800

    实现导航条搜索功能，可以搜索标题和简介

--- blog/post/urls.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-from django.conf.urls import url
-
-from . import views
-
-
-urlpatterns = [
-    url(r"^$", views.homepage_view, name="homepage"),
-]
--- blog/post/views.py
+++ blog/post/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, get_object_or_404
+from django.db.models import Q
 
 from .models import Post
 
@@ -13,3 +14,12 @@ def post_detail_view(request, slug):
     post = get_object_or_404(Post, slug=slug)
     return render(request, "post/detail.html",
                   {"post": post})
+
+
+def post_search_view(request):
+    query = request.GET.get("query", "")
+    posts = None
+    if query:
+        posts = Post.objects.filter(Q(title__icontains=query) | Q(description__icontains=query))
+    return render(request, "post/search.html",
+                  {"posts": posts, "query": query})
